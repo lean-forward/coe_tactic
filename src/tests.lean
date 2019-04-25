a696490455f0bd3c544d9cd6573505ab7805c009
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-04-25 17:20:17 +0200

    tests update

--- src/tests.lean
+++ src/tests.lean
@@ -8,45 +8,42 @@ import norm_cast lemmas_norm_cast
 constants (an bn cn dn : ℕ) (az bz cz dz : ℤ) (aq bq cq dq : ℚ)
 constants (ar br cr dr : ℝ) (ac bc cc dc : ℂ)
 
-example : (an : ℤ) = bn → an = bn := by {intro, assumption_mod_cast}
-example : an = bn → (an : ℤ) = bn := by {intro, assumption_mod_cast}
-example : az = bz ↔ (az : ℚ) = bz := by norm_cast1
+example : (an : ℤ) = bn → an = bn := λ h, by exact_mod_cast h -- by simp
+example : an = bn → (an : ℤ) = bn := λ h, by exact_mod_cast h -- by simp
+example : az = bz ↔ (az : ℚ) = bz := by norm_cast1 -- by simp
 
 example : (aq : ℝ) = br ↔ (aq : ℂ) = br := by norm_cast1
 example : (an : ℚ) = bz ↔ (an : ℂ) = bz := by norm_cast1
 example : (((an : ℤ) : ℚ) : ℝ) = bq ↔ ((an : ℚ) : ℂ) = (bq : ℝ) :=
-by {norm_cast1}
+by norm_cast1
 
-example : (an : ℤ) < bn ↔ an < bn             := by norm_cast1
+example : (an : ℤ) < bn ↔ an < bn             := by norm_cast1 -- by simp
 example : (an : ℚ) < bz ↔ (an : ℝ) < bz       := by norm_cast1
 example : ((an : ℤ) : ℝ) < bq ↔ (an : ℚ) < bq := by norm_cast1
 
 -- zero and one cause special problems
-example : 0 < (bq : ℝ) ↔ 0 < bq             := by norm_cast1
+example : 0 < (bq : ℝ) ↔ 0 < bq := by norm_cast1 -- by simp
+example : az > (1 : ℕ) ↔ az > 1 := by norm_cast1 -- by simp
+example : az > (0 : ℕ) ↔ az > 0 := by norm_cast1 -- by simp
+example : (an : ℤ) ≠ 0 ↔ an ≠ 0 := by norm_cast1 -- by simp
 example : aq < (1 : ℕ) ↔ (aq : ℝ) < (1 : ℤ) := by norm_cast1
-example : az > (1 : ℕ) ↔ az > 1 := by norm_cast1
-example : az > (0 : ℕ) ↔ az > 0 := by norm_cast1
-example : (an : ℤ) ≠ 0 ↔ an ≠ 0 := by norm_cast1
 
-example : (an : ℤ) + bn = (an + bn : ℕ)   := by norm_cast1
-example : (an : ℂ) + bq = ((an + bq) : ℚ) := by norm_cast1
-example : (((an : ℤ) : ℚ) : ℝ) + bn = (an + (bn : ℤ)) := by norm_cast1
+example : (an : ℤ) + bn = (an + bn : ℕ)   := by norm_cast1 -- by simp
+example : (an : ℂ) + bq = ((an + bq) : ℚ) := by norm_cast1 -- by simp
+example : (((an : ℤ) : ℚ) : ℝ) + bn = (an + (bn : ℤ)) := by norm_cast1 -- by simp
 
 example : (((((an : ℚ) : ℝ) * bq) + (cq : ℝ) ^ dn) : ℂ) = (an : ℂ) * (bq : ℝ) + cq ^ dn :=
-by norm_cast1
+by norm_cast1 -- by simp
 example : ((an : ℤ) : ℝ) < bq ∧ (cr : ℂ) ^ 2 = dz ↔ (an : ℚ) < bq ∧ ((cr ^ 2) : ℂ) = dz :=
 by norm_cast1
 
-example : (an : ℤ) = 1 → an = 1 := by {intro, assumption_mod_cast}
-example : (an : ℤ) < 5 → an < 5 := by {intro, assumption_mod_cast}
-example : an < 5 → (an : ℤ) < 5 := by {intro, assumption_mod_cast}
-example : (an + 5) < 10 → (an : ℤ) + 5 < 10 := by {intro, assumption_mod_cast}
-example : (an : ℤ) + 5 < 10 → (an + 5) < 10 := by {intro, assumption_mod_cast}
-example : ((an + 5 : ℕ) : ℤ) < 10 → an + 5 < 10 := by {intro, assumption_mod_cast}
-example : an + 5 < 10 → ((an + 5 : ℕ) : ℤ) < 10 := by {intro, assumption_mod_cast}
+example : (an : ℤ) = 1 → an = 1 := λ h, by exact_mod_cast h
+example : (an : ℤ) < 5 → an < 5 := λ h, by exact_mod_cast h
+example : an < 5 → (an : ℤ) < 5 := λ h, by exact_mod_cast h
+example : (an + 5) < 10 → (an : ℤ) + 5 < 10 := λ h, by exact_mod_cast h
+example : (an : ℤ) + 5 < 10 → (an + 5) < 10 := λ h, by exact_mod_cast h
+example : ((an + 5 : ℕ) : ℤ) < 10 → an + 5 < 10 := λ h, by exact_mod_cast h
+example : an + 5 < 10 → ((an + 5 : ℕ) : ℤ) < 10 := λ h, by exact_mod_cast h
 
 example (h : (cz : ℚ) = az / bz) : (cz : ℝ) = az / bz :=
-begin
-    rw_mod_cast [←rat.cast_coe_int az],
-    exact h
-end
+by rw_mod_cast [← rat.cast_coe_int az, h]
